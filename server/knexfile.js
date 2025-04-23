89719b9a81211067f611fb06d99d9c5d128486ca
LucianoVeiga <lucianoveigaa7@gmail.com>
2025-04-23 20:19:46 -0300

    fix: pg install

--- server/knexfile.js
+++ server/knexfile.js
@@ -4,8 +4,6 @@ require("dotenv").config();
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 
-console.log("ASD" + process.env.DATABASE_URI)
-
 module.exports = {
   development: {
     client: "pg",
